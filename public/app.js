296c06476e96a970a0f2dc259f1e6f3275e0583e
reverse <vyxlez@icloud.com>
2026-08-10 11:31:52 +0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -72,14 +72,14 @@ window.ADMFLIP = {
     }
   }
 
-  function escapeHTML(value) {
-    return String(value ?? "")
-      .replace(/&/g, "&amp;")
-      .replace(/</g, "&lt;")
-      .replace(/>/g, "&gt;")
-      .replace(/"/g, "&quot;")
-      .replace(/'/g, "&#039;");
-  }
+ function escapeHTML(value) {
+  return String(value ?? "")
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
 
   function formatNumber(value) {
     const n = Number(value);
